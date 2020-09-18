8b60ae219d5fa057404d99be51a9c85f67b9b260
Claire Tregunna <c.tregunna1980@gmail.com>
2020-09-18 21:30:58 +0100

    created a array of project information

---
+++ js/array.js
@@ -0,0 +1,50 @@
+const mockups = [
+    {
+        name: 'Momentum',
+        html: true,
+        sass: false,
+        javascript: false,
+    },
+    {
+        name: 'Firebase',
+        html: true,
+        sass: false,
+        javascript: false,
+    },
+    {
+        name: 'iOS',
+        html: true,
+        sass: false,
+        javascript: false,
+    },
+    {
+        name: 'Cloud Flare',
+        html: true,
+        sass: true,
+        javascript: false,
+    },
+    {
+        name: 'Social Network',
+        html: true,
+        sass: true,
+        javascript: false,
+    },
+    {
+        name: 'Paws Adoption Center',
+        html: true,
+        sass: true,
+        javascript: false,
+    },
+    {
+        name: 'ChatApp',
+        html: true,
+        sass: true,
+        javascript: true,
+    },
+    {
+        name: 'FeatherLITE',
+        html: true,
+        sass: true,
+        javascript: true,
+    },
+];
