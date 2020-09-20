ca8fc5297f7eb24d05728624a7b869d02c8a908a
Claire Tregunna <c.tregunna1980@gmail.com>
2020-09-20 15:01:49 +0100

    Create mockup content via JS

--- js/array.js
+++ js/array.js
@@ -10,7 +10,7 @@ const mockups = [
     {
         name: 'Firebase',
         image:'images/firebase.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a plugin they've been developing; fireboxjs. The design team has created a mockup for the landing page for this plugin and your boss needs you to code it out with just HTML and CSS!",
         html: true,
         sass: false,
         javascript: false,
@@ -18,7 +18,7 @@ const mockups = [
     {
         name: 'iOS',
         image:'images/ios.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "With this Mockup code challenge, I challenge you to take the iOS Settings as shown in the mockup and recreate it using HTML and CSS! This is a great time to use flexbox or even CSS Grid to structure your settings items.",
         html: true,
         sass: false,
         javascript: false,
@@ -26,7 +26,7 @@ const mockups = [
     {
         name: 'Cloud Flare',
         image:'images/cloudflare.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "CloudFlare is a cloud storage software that will allow users to securely store their files in the cloud. You will need to recreate the mockup provided to give this web space a login page.",
         html: true,
         sass: true,
         javascript: true,
@@ -34,7 +34,7 @@ const mockups = [
     {
         name: 'Social Network',
         image:'images/social.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "You're confident in your coding abilities and set off to build the next best social media platform! This mockup has a lot of similarities as Twitter. You'll be given a mockup for mobile devices to follow and execute with precision! You will supply your own profile picture as well as one of your recent achievements for the post that displays an achievement! Have fun with this and give it your best shot!",
         html: true,
         sass: true,
         javascript: false,
@@ -42,7 +42,7 @@ const mockups = [
     {
         name: 'Paws Adoption Center',
         image:'images/petadopt.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "You're taking your freelancing skills to the streets and noticed a pet adoption agency doesn't have a website. You offer to create one for them and sell it. They happily agree and tell you what they would like. Your job is to review the mockup and all images that the owner has provided for you. Try your best to recreate what they're asking for! You and the owner have agreed on a price but is willing to offer you more if you make it mobile friendly.The choice is up to you!",
         html: true,
         sass: true,
         javascript: false,
@@ -50,7 +50,7 @@ const mockups = [
     {
         name: 'ChatApp',
         image:'images/chatapp.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "You've been asked to create a chat interface! This will test your CSS skills by structuring multiple elements within a single container.",
         html: true,
         sass: true,
         javascript: true,
@@ -58,7 +58,7 @@ const mockups = [
     {
         name: 'FeatherLITE',
         image:'images/featherlite.png',
-        description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
+        description: "The design team at your company has come to you with some mockups for a potential note taking application your company wants to create; Feather. Feather will have two version LITE and PRO. You're tasked with creating the LITE version of this application from only these mockups and the tools you've gained during your Techdegree. Are you up for the task?",
         html: true,
         sass: true,
         javascript: true,
--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
 
-const project = document.querySelector('.mockup');
-let index = 0;
+// const project = document.querySelectorAll('.mockup');
+// let index = 0;
+
+// console.log(mockups);
+
+// mockups.forEach((mockup, index) => {
+//     project[index].innerHTML += `
+//                      <h2>${mockup.name}</h2>
+//                     <div class="img-contain">
+//                         <img src="${mockup.image}" alt="${mockup.name}">
+//                     </div>
+//                     <p>${mockup.description}</p>`;
+//                     index ++;
+// });
+
+const project = document.querySelector('.mockup-container');
 
 console.log(mockups);
 
-
-
 mockups.forEach(mockup => {
-    project.innerHTML += `<h2>${mockups[index].name}</h2>
-                    <img src="${mockups[index].image}" alt="${mockups[index].name}">
-                    <p>${mockups[index].description}</p>`;
-                    index ++;
+    const indivMockup = document.createElement('DIV');
+    const mockupHeader = document.createElement('H2');
+    const imgContainer = document.createElement('DIV');
+    const mockupDesc = document.createElement('P');
+    indivMockup.classList.add('mockup');
+    imgContainer.classList.add('img-contain');
+    mockupHeader.textContent = `${mockup.name}`;
+    imgContainer.innerHTML = `<img src="${mockup.image}" alt="${mockup.name}"></img>`;
+    mockupDesc.textContent = `${mockup.description}`;
+    indivMockup.appendChild(mockupHeader);
+    indivMockup.appendChild(imgContainer);
+    indivMockup.appendChild(mockupDesc);
+    project.appendChild(indivMockup);
+    console.log(mockupHeader);
+    console.log(indivMockup);
 });
-
-
-
-// projectHTML += `<h2>${mockup[index].name}</h2>
-// <img src="${mockup[index].image}" alt="${mockup[index].name}">
-// <p>${mockup[index].description}</p>`;
