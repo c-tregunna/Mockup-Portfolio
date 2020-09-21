15bed1fd2035c7cdb56eef03f606dc463fbdef01
Claire Tregunna <c.tregunna1980@gmail.com>
2020-09-21 20:11:25 +0100

    Changed images to fontawesome icons

--- js/array.js
+++ js/array.js
@@ -1,7 +1,7 @@
 const mockups = [
     {
         name: 'Momentum',
-        image:'images/momentum.png',
+        image:`<i class="far fa-comment"></i>`,
         description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a website they've been developing; Momentum. The design team has created a mockup for the beta testing landing page for this website and your boss needs you to code it out with just HTML and CSS!",
         html: true,
         sass: false,
@@ -9,7 +9,7 @@ const mockups = [
     },
     {
         name: 'Firebase',
-        image:'images/firebase.png',
+        image:`<i class="fas fa-fire"></i>`,
         description: "You're working for a company as a front-end developer. Your boss tells you that your team is putting the finishing touches on a plugin they've been developing; fireboxjs. The design team has created a mockup for the landing page for this plugin and your boss needs you to code it out with just HTML and CSS!",
         html: true,
         sass: false,
@@ -17,7 +17,7 @@ const mockups = [
     },
     {
         name: 'iOS',
-        image:'images/ios.png',
+        image:`<i class="fas fa-mobile-alt"></i>`,
         description: "With this Mockup code challenge, I challenge you to take the iOS Settings as shown in the mockup and recreate it using HTML and CSS! This is a great time to use flexbox or even CSS Grid to structure your settings items.",
         html: true,
         sass: false,
@@ -25,7 +25,7 @@ const mockups = [
     },
     {
         name: 'Cloud Flare',
-        image:'images/cloudflare.png',
+        image:`<i class="fab fa-cloudversify"></i>`,
         description: "CloudFlare is a cloud storage software that will allow users to securely store their files in the cloud. You will need to recreate the mockup provided to give this web space a login page.",
         html: true,
         sass: true,
@@ -33,7 +33,7 @@ const mockups = [
     },
     {
         name: 'Social Network',
-        image:'images/social.png',
+        image:`<i class="fas fa-user-friends"></i>`,
         description: "You're confident in your coding abilities and set off to build the next best social media platform! This mockup has a lot of similarities as Twitter. You'll be given a mockup for mobile devices to follow and execute with precision! You will supply your own profile picture as well as one of your recent achievements for the post that displays an achievement! Have fun with this and give it your best shot!",
         html: true,
         sass: true,
@@ -41,7 +41,7 @@ const mockups = [
     },
     {
         name: 'Paws Adoption Center',
-        image:'images/petadopt.png',
+        image:`<i class="fas fa-paw"></i>`,
         description: "You're taking your freelancing skills to the streets and noticed a pet adoption agency doesn't have a website. You offer to create one for them and sell it. They happily agree and tell you what they would like. Your job is to review the mockup and all images that the owner has provided for you. Try your best to recreate what they're asking for! You and the owner have agreed on a price but is willing to offer you more if you make it mobile friendly.The choice is up to you!",
         html: true,
         sass: true,
@@ -49,7 +49,7 @@ const mockups = [
     },
     {
         name: 'ChatApp',
-        image:'images/chatapp.png',
+        image:`<i class="far fa-comments"></i>`,
         description: "You've been asked to create a chat interface! This will test your CSS skills by structuring multiple elements within a single container.",
         html: true,
         sass: true,
@@ -57,10 +57,18 @@ const mockups = [
     },
     {
         name: 'FeatherLITE',
-        image:'images/featherlite.png',
+        image:`<i class="far fa-sticky-note"></i>`,
         description: "The design team at your company has come to you with some mockups for a potential note taking application your company wants to create; Feather. Feather will have two version LITE and PRO. You're tasked with creating the LITE version of this application from only these mockups and the tools you've gained during your Techdegree. Are you up for the task?",
         html: true,
         sass: true,
         javascript: true,
-    }
+    },
+    {
+        name: 'Treehouse landing page',
+        image:`<i class="fas fa-code"></i>`,
+        description: "***Coming Soon*** The project is based on the beloved Treehouse landing page for the techdegree. The page contains three widgets that you will manipulate with JavaScript to complete the functionality to the page. ",
+        html: false,
+        sass: false,
+        javascript: true,
+    },
 ];
--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
+//---------------------
+//Create mockup divs direct into HTML 2 different ways
+//---------------------
 
-// const project = document.querySelectorAll('.mockup');
-// let index = 0;
+//First method
+//***************
 
-// console.log(mockups);
-
-// mockups.forEach((mockup, index) => {
-//     project[index].innerHTML += `
-//                      <h2>${mockup.name}</h2>
-//                     <div class="img-contain">
-//                         <img src="${mockup.image}" alt="${mockup.name}">
-//                     </div>
-//                     <p>${mockup.description}</p>`;
-//                     index ++;
-// });
-
-const project = document.querySelector('.mockup-container');
+const project = document.querySelectorAll('.mockup');
 
 console.log(mockups);
 
-mockups.forEach(mockup => {
-    const indivMockup = document.createElement('DIV');
-    const mockupHeader = document.createElement('H2');
-    const imgContainer = document.createElement('DIV');
-    const mockupDesc = document.createElement('P');
-    indivMockup.classList.add('mockup');
-    imgContainer.classList.add('img-contain');
-    mockupHeader.textContent = `${mockup.name}`;
-    imgContainer.innerHTML = `<img src="${mockup.image}" alt="${mockup.name}"></img>`;
-    mockupDesc.textContent = `${mockup.description}`;
-    indivMockup.appendChild(mockupHeader);
-    indivMockup.appendChild(imgContainer);
-    indivMockup.appendChild(mockupDesc);
-    project.appendChild(indivMockup);
-    console.log(mockupHeader);
-    console.log(indivMockup);
+mockups.forEach((mockup, index) => {
+    project[index].innerHTML += `
+                     <h2>${mockup.name}</h2>
+                    <div class="img-contain">
+                        ${mockup.image}
+                    </div>
+                    <p class="mockup__desc">${mockup.description}</p>
+                    `;
 });
+
+//<img src="${mockup.image}" alt="${mockup.name}"></img>
+
+//Second method
+//***************
+
+// const project = document.querySelector('.mockup-container');
+
+// console.log(mockups);
+
+// mockups.forEach(mockup => {
+//     const indivMockup = document.createElement('DIV');
+//     const mockupHeader = document.createElement('H2');
+//     const imgContainer = document.createElement('DIV');
+//     const mockupDesc = document.createElement('P');
+//     indivMockup.classList.add('mockup');
+//     imgContainer.classList.add('img-contain');
+//     mockupHeader.textContent = `${mockup.name}`;
+//     imgContainer.innerHTML = `<img src="${mockup.image}" alt="${mockup.name}"></img>`;
+//     mockupDesc.textContent = `${mockup.description}`;
+//     indivMockup.appendChild(mockupHeader);
+//     indivMockup.appendChild(imgContainer);
+//     indivMockup.appendChild(mockupDesc);
+//     project.appendChild(indivMockup);
+//     console.log(mockupHeader);
+//     console.log(indivMockup);
+// });
+
+
+//---------------------
+// Select type of project buttons
+//---------------------
